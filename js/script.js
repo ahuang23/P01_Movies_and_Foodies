124216d609306b220a1d86ddf6be52a7bededa62
sdanielewicz <sdanielewicz@gmail.com>
2022-08-15 18:48:13 -0700

    added js path to index

---
+++ js/script.js
@@ -0,0 +1,10 @@
+var tmdbURL = "https://api.themoviedb.org/3/movie/550?api_key=eea5a34dde91516aaed29972492b8943"
+
+fetch(tmdbURL, {
+})
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data);
+  });
