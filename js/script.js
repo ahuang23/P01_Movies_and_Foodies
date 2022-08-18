9914c82a7bb676b4a377a65aa2aea2862afabcd2
sdanielewicz <sdanielewicz@gmail.com>
2022-08-17 22:39:01 -0700

    food calc WIP

--- js/script.js
+++ js/script.js
@@ -1,6 +1,4 @@
-
-
-
+var input = "Mystery";
 function movieCall(){
   var genre="";
   // var tmdbURL = "https://api.themoviedb.org/3/genre/movie/list?api_key=eea5a34dde91516aaed29972492b8943"; 
@@ -22,21 +20,45 @@ function movieCall(){
 
 
 
+
 }
 
-function yelpCall(selection){
+function yelpCall(input){
+  
+ 
   // const yelpHead = new Headers();
   // yelpHead.append({'Authorization:', 'Bearer SPNMpxXoVwWFRBSY8zqfCfCXLxqvHJ9XMZsyQW9uMP5mp7tQR-QMp3CVmg46fQIdTdZy78oZV6FThsPFPIHJ8azQxoe4jieedC7V_M5BEfZnctm4npIJ33W7oBf7YnYx'});
 
 //var yelpURL = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/categories";
+var movieArray = ["Action","Comedy","Horror","Mystery","Romance"];
 var foodArray = ["italian","french","bbq","fast food"];
+var food = {
+  "Action" : "fast food",
+  "Comedy" : ["prank1","prank2","prank3"],
+  "Horror" : "bbq",
+  "Mystery": ["one","two","three"],
+  "Romance" : "italian"
+}
 var cuisine;
 var yelpURL = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term="+cuisine+"&latitude=33.787914&longitude=-117.853104";
  
-if (selection = movies[0]){
-  
-}
+for (const [key,value] of Object.entries(food)){
+  if (key === input){
+    // console.log(food.Comedy);
+    if (input === food.Comedy){
+      rand = Math.floor(Math.random() * 3);
+      cuisine = food.Comedy[1];
+      console.log(cuisine);
 
+    }
+    else if (input === food.Mystery){
+      rand = Math.floor(Math.random() * 3);
+      cuisine = food.Mystery[1];
+      console.log(cuisine);
+    }
+    cuisine = key;
+  }
+}
 
   fetch(yelpURL, {
     method: 'GET',
@@ -56,6 +78,6 @@ if (selection = movies[0]){
 }
 
 movieCall();
-yelpCall();
+yelpCall(input);
 
 
