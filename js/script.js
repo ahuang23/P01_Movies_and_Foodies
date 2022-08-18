048598c4d7b7c83e62b2420ab92209cfdee1e8a0
sdanielewicz <sdanielewicz@gmail.com>
2022-08-17 21:34:09 -0700

    sucessful api endpoint and vars

--- js/script.js
+++ js/script.js
@@ -24,13 +24,20 @@ function movieCall(){
 
 }
 
-function groceryCall(){
+function yelpCall(selection){
   // const yelpHead = new Headers();
   // yelpHead.append({'Authorization:', 'Bearer SPNMpxXoVwWFRBSY8zqfCfCXLxqvHJ9XMZsyQW9uMP5mp7tQR-QMp3CVmg46fQIdTdZy78oZV6FThsPFPIHJ8azQxoe4jieedC7V_M5BEfZnctm4npIJ33W7oBf7YnYx'});
 
-//var yelpURL = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/categories"; 
-var yelpURL = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/categories/search?term=italian&location=chicago";
+//var yelpURL = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/categories";
+var foodArray = ["italian","french","bbq","fast food"];
+var cuisine;
+var yelpURL = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term="+cuisine+"&latitude=33.787914&longitude=-117.853104";
  
+if (selection = movies[0]){
+  
+}
+
+
   fetch(yelpURL, {
     method: 'GET',
     headers: new Headers({'Authorization':'Bearer Kgw9XiDAFyVaMDW0G_hv5b5VJqWZ1KMM4ARW4zQBbEd_hDVtGZqeRpuRGF9IXhwTJbC2IzabNk1lDpqhQtwe6XtjDsLfmvgJTUiDpucTb3-cUkcIgIWXGGUqHWL8YnYx', 
@@ -45,11 +52,10 @@ var yelpURL = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/busin
       console.log(data);
     });
 
-var foodArray = ["italian","french"]
 
 }
 
 movieCall();
-groceryCall();
+yelpCall();
 
 
