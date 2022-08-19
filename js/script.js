f28c56c8745d6c6fab180d941f74e566ded41d95
sdanielewicz <sdanielewicz@gmail.com>
2022-08-18 23:02:51 -0700

    api url complete

--- js/script.js
+++ js/script.js
@@ -1,20 +1,6 @@
-var input = "Mystery";
+var input = "Action";
 function movieCall(){
   var genre="";
-  // var tmdbURL = "https://api.themoviedb.org/3/genre/movie/list?api_key=eea5a34dde91516aaed29972492b8943"; 
-  // var tmdbURL = "https://api.themoviedb.org/3/genre/movie/discover?api_key=eea5a34dde91516aaed29972492b8943";
-//   var tmdbURL = "https://api.themoviedb.org/3/movie/550?api_key=eea5a34dde91516aaed29972492b8943";
-
-
-// fetch(tmdbURL, {
-// })
-//   .then(function (response) {
-//     return response.json();
-//   })
-//   .then(function (data) {
-//     console.log(data);
-//   });
-
 
 
 
@@ -30,52 +16,75 @@ function yelpCall(input){
   // yelpHead.append({'Authorization:', 'Bearer SPNMpxXoVwWFRBSY8zqfCfCXLxqvHJ9XMZsyQW9uMP5mp7tQR-QMp3CVmg46fQIdTdZy78oZV6FThsPFPIHJ8azQxoe4jieedC7V_M5BEfZnctm4npIJ33W7oBf7YnYx'});
 
 //var yelpURL = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/categories";
-var movieArray = ["Action","Comedy","Horror","Mystery","Romance"];
-var foodArray = ["italian","french","bbq","fast food"];
+
+
 var food = {
   "Action" : "fast food",
-  "Comedy" : ["prank1","prank2","prank3"],
+  "Comedy" : ["sandwich","pizza", "ice cream"],
   "Horror" : "bbq",
-  "Mystery": ["one","two","three"],
-  "Romance" : "italian"
+  "Mystery": ["hot dog","Italian",""],
+  "Romance" : "French"
 }
 var cuisine;
-var yelpURL = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term="+cuisine+"&latitude=33.787914&longitude=-117.853104";
  
-for (const [key,value] of Object.entries(food)){
-  if (key === input){
-    // console.log(food.Comedy);
-    if (input === food.Comedy){
-      rand = Math.floor(Math.random() * 3);
-      cuisine = food.Comedy[1];
-      console.log(cuisine);
 
-    }
-    else if (input === food.Mystery){
-      rand = Math.floor(Math.random() * 3);
-      cuisine = food.Mystery[1];
-      console.log(cuisine);
-    }
-    cuisine = key;
+
+for (var prop in food){
+  var rand = Math.floor(Math.random() * 3);
+ if ((prop === input) && (prop === "Mystery")){
+    cuisine = food.Mystery[rand];
+ }
+ else if ((prop === input) && (prop === "Comedy")){
+    cuisine = food.Comedy[rand];
+ }
+  else if (prop === input){
+    cuisine = food[prop];
   }
+  
 }
+
+console.log(cuisine);
+
+var randRest = Math.floor(Math.random() * 20);
+
+
+
+
+
+var yelpURL = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term="+cuisine+"&latitude=33.787914&longitude=-117.853104";
+
+
 
   fetch(yelpURL, {
     method: 'GET',
     headers: new Headers({'Authorization':'Bearer Kgw9XiDAFyVaMDW0G_hv5b5VJqWZ1KMM4ARW4zQBbEd_hDVtGZqeRpuRGF9IXhwTJbC2IzabNk1lDpqhQtwe6XtjDsLfmvgJTUiDpucTb3-cUkcIgIWXGGUqHWL8YnYx', 
   'Content-Type':'application/json'})
   })
-  // ,'Access-Control-Allow-Origin':'*'
-
     .then(function (response) {
       return response.json();
     })
     .then(function (data) {
       console.log(data);
+      var pick = data.businesses[randRest];
+      var restuarant = pick.name;
+      var address = pick.location;
+      var imgURL = pick.image_url;
+
+      console.log(pick);
+      console.log(restuarant);
+      console.log(address);
+      console.log(imgURL); 
     });
 
 
 }
+
+
+      // var iconForNow = new Image(getForIcon);
+      // iconForNow.src = "https://openweathermap.org/img/wn/" + getForIcon + "@2x.png";
+      // iconForNow.width = 100;
+
+
 
 movieCall();
 yelpCall(input);
