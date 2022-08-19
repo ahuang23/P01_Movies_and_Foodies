a515a4cd5bc0a01f6525536d87737000f9968d3c
AnthonyMartinez <anthonymartienz0423@gmail.com>
2022-08-18 19:37:53 -0700

    fix submit button

--- assets/storage.js
+++ assets/storage.js
@@ -1,1 +1,12 @@
-//onsole.log(localStorage);
+//Save results to local storage
+
+var movieInfo = localStorage.getItem("movieInfo");
+if (movieInfo === null){
+
+//if no movieInfo, create a new array
+movieInfo = [];
+
+//Parse movieInfo
+} else {
+  movieInfo = JSON.parse(movieInfo);
+}
