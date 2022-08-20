d4c5bcf15429449fe1c7c1a8f572e2ee5d591a94
sdanielewicz <sdanielewicz@gmail.com>
2022-08-20 14:33:11 -0700

    food content on page

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 var submit = $(".submit");
 var reset = $(".reset");
 var section = $(".section");
+var section2 = $(".section2");
 var movieSelection;
 var input;
 var selectedNew = $("#select1").on("change", function(){
@@ -15,19 +16,20 @@ reset.click(function(){
 })
 
 
-function yelpCall(input){
-
+function yelpCall(){
 var food = {
   "Action" : "fast food",
   "Comedy" : ["sandwich","pizza", "ice cream"],
   "Horror" : "bbq",
-  "Mystery": ["hot dog","Italian",""],
-  "Romance" : "French"
+  "Mystery": ["hot dog","wings","diner"],
+  "Romance" : "french"
 }
 var cuisine;
- 
+var rand = Math.floor(Math.random() * 2);
+var randRest = Math.floor(Math.random() * 10);
+
 for (var prop in food){
-  var rand = Math.floor(Math.random() * 3);
+  
  if ((prop === input) && (prop === "Mystery")){
     cuisine = food.Mystery[rand];
  }
@@ -40,11 +42,8 @@ for (var prop in food){
   
 }
 
-console.log(cuisine);
-
-var randRest = Math.floor(Math.random() * 20);
-
-var yelpURL = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term="+cuisine+"&latitude=33.787914&longitude=-117.853104";
+var yelpURL = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term="+cuisine+"&latitude=33.787914&longitude=-117.853104&limit=10";
+// var yelpURL = 'http://www.whateverorigin.org/get?url=' + encodeURIComponent('https://api.yelp.com/v3/businesses/search?term="+cuisine+"&latitude=33.787914&longitude=-117.853104');
 
   fetch(yelpURL, {
     method: 'GET',
@@ -58,16 +57,42 @@ var yelpURL = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/busin
       console.log(data);
       var pick = data.businesses[randRest];
       var restuarant = pick.name;
-      var address = pick.location;
+      var address = {
+        "address1" : pick.location.address1,
+        "address2" : pick.location.address2,
+        "city" : pick.location.city,
+        "state" : pick.location.state,
+        "zip" : pick.location.zip_code,
+        "phone" : pick.phone
+      };
       var imgURL = pick.image_url;
-
-      console.log(pick);
-      console.log(restuarant);
       console.log(address);
       console.log(imgURL); 
+
+      section2.empty();
+      
+      var pickImg = new Image();
+      pickImg.src = imgURL
+      pickImg.height = 400;
+
+      var phoneFormat = address.phone.slice(-10);
+      
+      section2.append(pickImg);
+      section2.append("<br>")
+      section2.append('<span style="font-weight:bold;font-size:30px">' + restuarant + '</span>')
+      section2.append("<br>")
+      section2.append(address.address1);
+      section2.append("<br>")
+      if (address.address2){
+      section2.append(address.address2);
+      section2.append("<br>")
+      }
+      
+      section2.append(address.city+", "+address.state+" "+address.zip);
+      section2.append("<br>")
+      section2.append(phoneFormat);
+      
     });
-
-
 }
 
 
@@ -92,16 +117,16 @@ function movieCall(){
   } 
   
   var moviesList = "https://api.themoviedb.org/3/discover/movie?api_key=eea5a34dde91516aaed29972492b8943&language=en-US&with_genres=" + movieSelection;
-  console.log(moviesList);
+  // console.log(moviesList);
   fetch (moviesList, {
   })
     .then(function (response){
       return response.json();
     })
     .then(function (data){
-      console.log(data);
+      // console.log(data);
     var randomNum = Math.floor(Math.random() * 10);
-    console.log(randomNum);
+    // console.log(randomNum);
     var moviesSelected = {
       titles: data.results[randomNum].title,
       overview: data.results[randomNum].overview,
@@ -114,9 +139,9 @@ function movieCall(){
   section.append("<br>")
   section.append(moviesSelected.overview);
   
-  console.log(moviesSelected.titles); // append to html class
-  console.log(moviesSelected.overview); // append to html class
-  console.log(moviesSelected.poster); // append to html class
+  // console.log(moviesSelected.titles); // append to html class
+  // console.log(moviesSelected.overview); // append to html class
+  // console.log(moviesSelected.poster); // append to html class
   // var imageLink = 
   // console.log(imageLink);
   // Save results to local storage
