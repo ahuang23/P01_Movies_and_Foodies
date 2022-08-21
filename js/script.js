d013bbf816bcdaeac75c85d83806895b481407d4
sdanielewicz <sdanielewicz@gmail.com>
2022-08-20 17:26:34 -0700

    comment cors out

--- js/script.js
+++ js/script.js
@@ -62,7 +62,8 @@ for (var prop in food){
   }
 }
 
-var yelpURL = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term="+cuisine+"&price="+price+"&latitude=33.787914&longitude=-117.853104&limit=10";
+// https://cors-anywhere.herokuapp.com/
+var yelpURL = "https://api.yelp.com/v3/businesses/search?term="+cuisine+"&price="+price+"&latitude=33.787914&longitude=-117.853104&limit=10";
  
 fetch(yelpURL, {
     method: 'GET',
